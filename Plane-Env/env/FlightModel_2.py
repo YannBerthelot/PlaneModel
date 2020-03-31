13d3fa33b0e384b6565947ecc997546ef687f83c
Yann BERTHERLOT <yann.berthelot@capgemini.com>
2020-03-31 16:41:58 +0200

    Improved but unstable simulation

--- Plane-Env/env/FlightModel_2.py
+++ Plane-Env/env/FlightModel_2.py
@@ -1,7 +1,10 @@
 import numpy as np
 import math
+from math import cos, sin
 import matplotlib.pyplot as plt
 import pandas as pd
+from numpy import arcsin
+from numpy.linalg import norm
 
 
 class FlightModel:
@@ -10,20 +13,22 @@ class FlightModel:
         self.g = 9.81
         self.m = 73500
         self.RHO = 1.225
-        self.S_x = 12
-        self.S_z = 120
-        self.C_x = 0.03
-        self.C_z = 0.5
+        self.S_front = 12
+        self.S_wings = 120
+        self.C_x_min = 0.08
+        self.C_z_max = 1.5
         self.THRUST_MAX = 113000 * 2
         self.DELTA_T = 1
-
+        self.V_1 = 150
+        self.V_1_ok = False
         # DYNAMICS
-        self.A = [0, 0]
-        self.V = [0, 0]
-        self.Pos = [0, 0]
+        self.A = [(0), (0)]
+        self.V = [(0), (0)]
+        self.Pos = [(0), (0)]
+        self.theta = 0
 
         # LISTS_FOR_PLOT
-        self.lift_vec = []
+        self.lift_vec = [[], []]
         self.P_vec = []
         self.T_vec = [[], []]
         self.drag_vec = [[], []]
@@ -31,6 +36,9 @@ class FlightModel:
         self.V_vec = [[], []]
         self.Pos_vec = [[], []]
         self.alt_factor_vec = []
+        self.alpha_vec = []
+        self.gamma_vec = []
+        self.theta_vec = []
 
         # KPIS
         self.max_alt = 0
@@ -40,53 +48,99 @@ class FlightModel:
         self.min_V = 0
 
     def drag(self, S, V, C):
-        return 0.5 * self.RHO * S * C * (V ** 2)
+        print("S", S, "C", C, "V", V)
+        return 0.5 * self.RHO * S * C * np.power(V, 2)
+
+    def C_x(self, alpha):
+        return (np.degrees(alpha) * 0.02) ** 2 + self.C_x_min
+
+    def C_z(self, alpha):
+        if np.degrees(alpha) < 15:
+            print("alpha", np.degrees(alpha))
+            return (np.degrees(alpha) / 15) * self.C_z_max
+        elif np.degrees(alpha) < 20:
+            return 1.5 - ((np.degrees(alpha) - 15) / 15) * 1.5
+        else:
+            ##Décrochage
+            print("décrochage")
+            return 0
+
+    def gamma(self):
+        if norm(self.V) > 0:
+            gamma = arcsin(self.V[1] / norm(self.V))
+            return gamma
+        else:
+            return 0
+
+    def alpha(self, gamma):
+        alpha = self.theta - gamma
+        return alpha
+
+    def S_x(self, alpha):
+        return cos(alpha) * self.S_front + sin(alpha) * self.S_wings
+
+    def S_z(self, alpha):
+        return sin(alpha) * self.S_front + cos(alpha) * self.S_wings
 
     def check_colisions(self):
         return self.Pos[1] <= 0
 
-    def compute_acceleration(self, thrust, alpha):
-        V_x = self.V[0]
-        V_z = self.V[1]
+    def compute_acceleration(self, thrust):
+        V = norm(self.V)
+        gamma = self.gamma()
+        alpha = self.alpha(gamma)
 
         # Compute P
         P = self.m * self.g
         # Compute surfaces according to alpha
-        S_x = math.cos(alpha) * self.S_x + math.sin(alpha) * self.S_z
-        S_z = math.sin(alpha) * self.S_x + math.cos(alpha) * self.S_z
-        # Compute horizontal and vertical Drags
-        drag_x = self.drag(S_x, V_x, self.C_x)
-        drag_z = self.drag(S_z, V_z, self.C_z)
-        # Compute lift (same formula as draft but the speed is the one perpendicular to the surface)
-        lift = self.drag(S_x, V_x, self.C_z)
 
+        # Compute total Drag
+        drag = self.drag(self.S_x(alpha), V, self.C_x(alpha))
+
+        # Compute lift
+        lift = self.drag(self.S_z(alpha), V, self.C_z(alpha))
+        print("lift", lift)
         # Compute sum of forces for each axis
         # Z-Axis
-        F_z = abs(lift) - abs(P) + abs(thrust) * math.sin(alpha) - drag_z
-        if self.check_colisions() and F_z <= 0:
-            F_z = 0
-            energy = 0.5 * self.m * self.V[1] ** 2
-            self.V[1] = 0
-            self.Pos[1] = 0
+        lift_z = abs(cos(self.theta) * lift)
+        drag_z = -sin(gamma) * drag
+        thrust_z = sin(self.theta) * thrust
+        F_z = lift_z + drag_z + thrust_z - P
+        print("F_Z", F_z, "dragz", drag_z, "thrust_z", thrust_z, "lift_z", lift_z)
 
         # X-Axis
-        F_x = abs(thrust) * math.cos(alpha) - abs(drag_x)
+        lift_x = -sin(self.theta) * lift
+        drag_x = -abs(cos(gamma) * drag)
+        thrust_x = cos(self.theta) * thrust
+        F_x = lift_x + drag_x + thrust_x
 
+        print("F_x", F_x, "lift_x", lift_x, "thurst_x", thrust_x)
+
+        if self.check_colisions() and F_z <= 0:
+            print("colision")
+            F_z = 0
+            # energy = 0.5 * self.m * V ** 2
+            self.V[1] = 0
+            self.Pos[1] = 0
         # Compute Acceleration using sum of forces = m.a
         A = [F_x / self.m, F_z / self.m]
 
-        self.lift_vec.append(lift)
+        self.lift_vec[0].append(lift_x)
+        self.lift_vec[1].append(lift_z)
         self.P_vec.append(P)
-        self.T_vec[1].append(thrust * math.sin(alpha))
-        self.T_vec[0].append(thrust * math.cos(alpha))
+        self.T_vec[1].append(thrust_z)
+        self.T_vec[0].append(thrust_x)
         self.drag_vec[0].append(drag_x)
         self.drag_vec[1].append(drag_z)
+        self.alpha_vec.append(np.degrees(alpha))
+        self.gamma_vec.append(np.degrees(gamma))
+        self.theta_vec.append(np.degrees(self.theta))
 
         return A
 
-    def compute_dyna(self, thrust, alpha):
+    def compute_dyna(self, thrust):
         # Update acceleration, speed and position
-        self.A = self.compute_acceleration(thrust, alpha)
+        self.A = self.compute_acceleration(thrust)
         self.V = [self.V[i] + self.A[i] * self.DELTA_T for i in range(2)]
         self.Pos = [self.Pos[i] + self.V[i] * self.DELTA_T for i in range(2)]
 
@@ -105,15 +159,23 @@ class FlightModel:
         print("max V", self.max_V)
         print("min V", self.min_V)
 
-    def compute_episodes(self, thrust, alpha, num_episodes):
+    def compute_episodes(self, thrust, theta, num_episodes):
+        self.theta = np.radians(theta)
         # Change alpha from rad to deg
-        alpha = alpha / 180
-
         for i in range(num_episodes):
+            print("i", i)
+            # if self.Pos[1] < 4000:
+            #     if self.V[0] > self.V_1:
+            #         self.V_1_ok = True
+            #     if self.V_1_ok:
+            #         self.theta = np.radians(15)
+            #     else:
+            #         self.theta = np.radians(0)
+            # else:
+            #     self.theta = np.radians(5)
             # Apply the atitude factor to the thrust
-            thrust_modified = thrust * self.altitude_factor()
-
-            self.compute_dyna(thrust_modified, alpha)
+            thrust_modified = thrust
+            self.compute_dyna(thrust_modified)
         self.plot_graphs()
         self.max_alt = max(self.Pos_vec[1])
         self.max_A = [max(self.A_vec[0]), max(self.A_vec[1])]
@@ -124,15 +186,32 @@ class FlightModel:
 
     def altitude_factor(self):
         alt = self.Pos[1]
-        a = 1 / (math.exp(alt / 60000))
-        self.alt_factor_vec.append(a)
+        a = 1 / (math.exp(alt / 100000))
+        self.alt_factor_vec.append(a ** 0.7)
+        a = 1
         return max(0, min(1, a ** (0.7)))
 
     def plot_graphs(self):
         # Z-axis
         Force_vec_z = [element * self.m for element in self.A_vec[1]]
 
-        ax = pd.Series(self.lift_vec).plot(label="Lift")
+        y = pd.Series(self.drag_vec[1])
+        x = pd.Series(self.V_vec[1])
+        plt.scatter(x, y)
+        plt.title("Drag z vs Vz")
+        plt.autoscale()
+        plt.legend()
+        plt.show()
+
+        ax = pd.Series(self.alpha_vec).plot(label="Alpha")
+        pd.Series(self.gamma_vec).plot(label="Gamma", ax=ax)
+        pd.Series(self.theta_vec).plot(label="Theta", ax=ax)
+        plt.title("Angles")
+        plt.autoscale()
+        plt.legend()
+        plt.show()
+
+        ax = pd.Series(self.lift_vec[1]).plot(label="Lift z")
         pd.Series(self.P_vec).plot(label="P", ax=ax)
         pd.Series(self.T_vec[1]).plot(label="Thrust z", ax=ax)
         pd.Series(self.drag_vec[1]).plot(label="Drag z", ax=ax)
@@ -145,6 +224,7 @@ class FlightModel:
         Force_vec_x = [element * self.m for element in self.A_vec[0]]
         ax = pd.Series(self.T_vec[0]).plot(label="Thrust x")
         pd.Series(self.drag_vec[0]).plot(label="Drag x", ax=ax)
+        pd.Series(self.lift_vec[0]).plot(label="Lift x", ax=ax)
         pd.Series(Force_vec_x).plot(label="Total x", ax=ax)
         plt.title("Xaxis")
         plt.autoscale()
@@ -200,5 +280,5 @@ class FlightModel:
 
 
 Model = FlightModel()
-Model.compute_episodes(113000 * 2, 0, 750)
+Model.compute_episodes(113000 * 2, 10, 2500)
 
